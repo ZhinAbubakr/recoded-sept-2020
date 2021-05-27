449e271aa2b4b60b8cf79b5c517af2b85cb12a45
zhin <zheen99abubakr@gmail.com>
2021-05-27 23:23:15 +0300

    test

--- site/bulletin_board/public/javascripts/endpoints.js
+++ site/bulletin_board/public/javascripts/endpoints.js
@@ -1,38 +1,42 @@
-
 // Add information in profile
 
-function Editing_profiles(firstName, lastName,birthdate,bio,gender ,callback) {
+function Editing_profiles(
+  firstName,
+  lastName,
+  birthdate,
+  bio,
+  gender,
+  callback
+) {
   var Editing = {
     firstName: firstName,
     lastName: lastName,
-    birthdate:birthdate, 
-    bio:bio,
-    gender:gender
+    birthdate: birthdate,
+    bio: bio,
+    gender: gender,
   };
 
   $.ajax({
-      type: "POST",
-      url: "/users/Editing_profiles",
-      data: JSON.stringify(Editing),
-      contentType: "application/json; charset=utf-8",
-      dataType: "json",
-      success: function(result){
-        callback({
-          success: true,
-          redirect_uri: result.redirect_uri
-        });
-      },
-      error: function(error) {
-        callback({
-          success: false,
-          redirect_uri: null,
-          error_message: 'Error '
-          
-        });
-      }
+    type: "POST",
+    url: "/users/Editing_profiles",
+    data: JSON.stringify(Editing),
+    contentType: "application/json; charset=utf-8",
+    dataType: "json",
+    success: function (result) {
+      callback({
+        success: true,
+        redirect_uri: result.redirect_uri,
+      });
+    },
+    error: function (error) {
+      callback({
+        success: false,
+        redirect_uri: null,
+        error_message: "Error ",
+      });
+    },
   });
 }
-
 
 // Posts
 
@@ -52,28 +56,28 @@ function Editing_profiles(firstName, lastName,birthdate,bio,gender ,callback) {
 function create_post(title, message, callback) {
   var post = {
     title: title,
-    message: message
+    message: message,
   };
 
   $.ajax({
-      type: "POST",
-      url: "/posts/",
-      data: JSON.stringify(post),
-      contentType: "application/json; charset=utf-8",
-      dataType: "json",
-      success: function(result){
-        callback({
-          success: true,
-          redirect_uri: result.redirect_uri
-        });
-      },
-      error: function(error) {
-        callback({
-          success: false,
-          redirect_uri: null,
-          error_message: error.responseJSON.error_message
-        });
-      }
+    type: "POST",
+    url: "/posts/",
+    data: JSON.stringify(post),
+    contentType: "application/json; charset=utf-8",
+    dataType: "json",
+    success: function (result) {
+      callback({
+        success: true,
+        redirect_uri: result.redirect_uri,
+      });
+    },
+    error: function (error) {
+      callback({
+        success: false,
+        redirect_uri: null,
+        error_message: error.responseJSON.error_message,
+      });
+    },
   });
 }
 
@@ -84,18 +88,19 @@ function create_post(title, message, callback) {
  * There is no callback for this function - the website will assume it was
  * successful.
  */
+
 function upvote(id, state) {
   var vote = {
-    upvoted: state
+    upvoted: state,
   };
 
   $.ajax({
-      type: "POST",
-      url: "/posts/" + id + "/upvotes",
-      data: JSON.stringify(vote),
-      contentType: "application/json; charset=utf-8",
-      dataType: "json"
-    });
+    type: "POST",
+    url: "/posts/" + id + "/upvotes",
+    data: JSON.stringify(vote),
+    contentType: "application/json; charset=utf-8",
+    dataType: "json",
+  });
 }
 
 // Users
@@ -116,28 +121,28 @@ function upvote(id, state) {
 function signup(username, password, callback) {
   var credentials = {
     username: username,
-    password: password
+    password: password,
   };
 
   $.ajax({
-      type: "POST",
-      url: "/users/",
-      data: JSON.stringify(credentials),
-      contentType: "application/json; charset=utf-8",
-      dataType: "json",
-      success: function(result){
-        callback({
-          success: true,
-          redirect_uri: result.redirect_uri
-        });
-      },
-      error: function(error) {
-        callback({
-          success: false,
-          redirect_uri: null,
-          error_message: error.responseJSON.error_message
-        });
-      }
+    type: "POST",
+    url: "/users/",
+    data: JSON.stringify(credentials),
+    contentType: "application/json; charset=utf-8",
+    dataType: "json",
+    success: function (result) {
+      callback({
+        success: true,
+        redirect_uri: result.redirect_uri,
+      });
+    },
+    error: function (error) {
+      callback({
+        success: false,
+        redirect_uri: null,
+        error_message: error.responseJSON.error_message,
+      });
+    },
   });
 }
 
@@ -157,116 +162,109 @@ function signup(username, password, callback) {
 function login(username, password, callback) {
   var credentials = {
     username: username,
-    password: password
+    password: password,
   };
 
   $.ajax({
-      type: "POST",
-      url: "/users/login",
-      data: JSON.stringify(credentials),
-      contentType: "application/json; charset=utf-8",
-      dataType: "json",
-      success: function(result){
-        callback({
-          success: true,
-          redirect_uri: result.redirect_uri
-        });
-      },
-      error: function(error) {
-        callback({
-          success: false,
-          redirect_uri: null,
-          error_message: error.responseJSON.error_message
-        });
-      }
+    type: "POST",
+    url: "/users/login",
+    data: JSON.stringify(credentials),
+    contentType: "application/json; charset=utf-8",
+    dataType: "json",
+    success: function (result) {
+      callback({
+        success: true,
+        redirect_uri: result.redirect_uri,
+      });
+    },
+    error: function (error) {
+      callback({
+        success: false,
+        redirect_uri: null,
+        error_message: error.responseJSON.error_message,
+      });
+    },
   });
 }
 
-
 /* changing password */
 
-function edit_password(new_password, old_password,confirm_password, callback) {
+function edit_password(new_password, old_password, confirm_password, callback) {
   var user_password = {
     oldPassword: old_password,
     newPassword: new_password,
-    confirmPassword : confirm_password
+    confirmPassword: confirm_password,
   };
   $.ajax({
-      type: "POST",
-      url: "/users/edit_password/",
-      data: JSON.stringify(user_password),
-      contentType: "application/json; charset=utf-8",
-      dataType: "json",
-      success: function(result){
-        callback({
-          success: true,
-          redirect_uri: result.redirect_uri
-        });
-      },
-      error: function(error) {
-        callback({
-          success: false,
-          redirect_uri: result.redirect_uri,
-          error_message: error.responseJSON.error_message
-        });
-      }
-  }
-  
-  )
-  ;
+    type: "POST",
+    url: "/users/edit_password/",
+    data: JSON.stringify(user_password),
+    contentType: "application/json; charset=utf-8",
+    dataType: "json",
+    success: function (result) {
+      callback({
+        success: true,
+        redirect_uri: result.redirect_uri,
+      });
+    },
+    error: function (error) {
+      callback({
+        success: false,
+        redirect_uri: result.redirect_uri,
+        error_message: error.responseJSON.error_message,
+      });
+    },
+  });
 }
 
-function create_comment(commentt,post_id,callback){
-
-  let  comment={
-    comment:commentt
-  }
+function create_comment(commentt, post_id, callback) {
+  let comment = {
+    comment: commentt,
+  };
 
   $.ajax({
-    type: 'POST',
-    url: "/posts/"+post_id+"/comments",
+    type: "POST",
+    url: "/posts/" + post_id + "/comments",
     data: JSON.stringify(comment),
     contentType: "application/json; charset=utf-8",
     dataType: "json",
-    success: function(data) {
-             callback(data)
-     },
-    error: function(error){
-      callback(error)
-    }
-  })
-}
-
-function remove_comment(comment_id,callback){
-  $.ajax({
-    type: 'DELETE',
-    url: "/posts/comments/"+comment_id,
-    success: function() {
-      callback()
+    success: function (data) {
+      callback(data);
     },
-    error: function(error){
-      callback(error)
-    }
-  })
+    error: function (error) {
+      callback(error);
+    },
+  });
 }
 
+function remove_comment(comment_id, callback) {
+  $.ajax({
+    type: "DELETE",
+    url: "/posts/comments/" + comment_id,
+    success: function () {
+      callback();
+    },
+    error: function (error) {
+      callback(error);
+    },
+  });
+}
 
-function edit_comment(comment_id,commentMessage,callback){
-  let  comment={
-       comment:commentMessage
-    }
-       $.ajax({
-       type: 'PUT', 
-       url: '/posts/comments/'+comment_id,
-       data: JSON.stringify(comment),
-       contentType: "application/json; charset=utf-8",
-       dataType: "json",
-       success: function(newComment) {
-       callback(newComment)
-       },
-       error: function(error){
-        callback(error)
-      }
-     })
-    }
-  
+function edit_comment(comment_id, commentMessage, callback) {
+  let comment = {
+    comment: commentMessage,
+  };
+  $.ajax({
+    type: "PUT",
+    url: "/posts/comments/" + comment_id,
+    data: JSON.stringify(comment),
+    contentType: "application/json; charset=utf-8",
+    dataType: "json",
+    success: function (newComment) {
+      callback(newComment);
+    },
+    error: function (error) {
+      callback(error);
+    },
+  });
+}
